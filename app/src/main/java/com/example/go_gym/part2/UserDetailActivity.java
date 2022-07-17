9b96c1d7f68df02e959430c47929471921125a39
acbgo <1105717400@qq.com>
2022-07-17 16:30:57 +0800

    7.17
    增加蓝牙控制

--- app/src/main/java/com/example/go_gym/part2/UserDetailActivity.java
+++ app/src/main/java/com/example/go_gym/part2/UserDetailActivity.java
@@ -1,18 +1,37 @@
 package com.example.go_gym.part2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.go_gym.R;
 import com.example.go_gym.SelectActivity;
 
-public class UserDetailActivity extends AppCompatActivity {
-    TextView push_up_old,push_up_new,squat_old,squat_new,plank_old,plank_new;
+import java.util.Set;
 
+public class UserDetailActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 10;//其是需要自己定义的局部常量。
+    private BluetoothAdapter mBluetoothAdapter;
+    TextView push_up_old,push_up_new,squat_old,squat_new,plank_old,plank_new;
+    Button blueTooth_check;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +47,8 @@ public class UserDetailActivity extends AppCompatActivity {
         squat_new = findViewById(R.id.squat_new);
         plank_old = findViewById(R.id.plank_old);
         plank_new = findViewById(R.id.plank_new);
+        blueTooth_check = findViewById(R.id.BlueTooth_check);
+
 
         push_up_old.setOnClickListener(view -> {
             Intent intent0 = new Intent();
@@ -55,6 +76,41 @@ public class UserDetailActivity extends AppCompatActivity {
         plank_new.setOnClickListener(view -> {
 
         });
+
+        blueTooth_check.setOnClickListener(view -> {
+            //查看设备是否支持蓝牙
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter == null) {
+                Toast.makeText(this, "设备不支持蓝牙！", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "设备支持蓝牙！", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        blueTooth_check.setOnClickListener(view -> {
+            if (!mBluetoothAdapter.isEnabled()) {
+                //向系统请求开启蓝牙
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);//结果返回回调到onActivityResult()
+            }else {
+                //已经开启了蓝牙
+                Toast.makeText(this,"蓝牙已经开启",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_ENABLE_BT){
+            Toast.makeText(this,"蓝牙已经开启",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
