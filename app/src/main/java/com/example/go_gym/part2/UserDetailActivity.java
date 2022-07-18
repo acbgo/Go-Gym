fb4dd38d391276b9b73f4d9c6978e1a472fb18fa
acbgo <1105717400@qq.com>
2022-07-18 12:32:53 +0800

    添加菜单
    在右上角添加了菜单选项，以便控制蓝牙状态，同时避免了多个按钮影响美观问题

--- app/src/main/java/com/example/go_gym/part2/UserDetailActivity.java
+++ app/src/main/java/com/example/go_gym/part2/UserDetailActivity.java
@@ -1,36 +1,28 @@
 package com.example.go_gym.part2;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.WindowManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.go_gym.R;
 import com.example.go_gym.SelectActivity;
-
-import java.util.Set;
 
 public class UserDetailActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 10;//其是需要自己定义的局部常量。
     private BluetoothAdapter mBluetoothAdapter;
 
-    TextView push_up_old,push_up_new,squat_old,squat_new,plank_old,plank_new;
-    Button blueTooth_check,turn_on,turn_off;
+    TextView push_up_old, push_up_new, squat_old, squat_new, plank_old, plank_new;
+    Button bt_menu;
     Toast myToast;
 
     @SuppressLint("MissingPermission")
@@ -39,6 +31,7 @@ public class UserDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
 
+
         //获取主要的控件
         push_up_old = findViewById(R.id.push_up_old);
         push_up_new = findViewById(R.id.push_up_new);
@@ -46,14 +39,12 @@ public class UserDetailActivity extends AppCompatActivity {
         squat_new = findViewById(R.id.squat_new);
         plank_old = findViewById(R.id.plank_old);
         plank_new = findViewById(R.id.plank_new);
-        blueTooth_check = findViewById(R.id.BlueTooth_check_support);
-        turn_on = findViewById(R.id.turn_on);
-        turn_off = findViewById(R.id.turn_off);
+        bt_menu = findViewById(R.id.button_bt);
 
 
         push_up_old.setOnClickListener(view -> {
             Intent intent0 = new Intent();
-            intent0.setClass(UserDetailActivity.this,PlayActivity.class);
+            intent0.setClass(UserDetailActivity.this, PlayActivity.class);
             startActivity(intent0);
             finish();
         });
@@ -78,47 +69,75 @@ public class UserDetailActivity extends AppCompatActivity {
 
         });
 
-        blueTooth_check.setOnClickListener(view -> {
-            //查看设备当前蓝牙状态
-            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-            if (mBluetoothAdapter == null) {
-                showToast("设备不支持蓝牙！");
-            } else {
-                if (mBluetoothAdapter.isEnabled())
-                    showToast("蓝牙状态为打开");
-                else
-                    showToast("蓝牙状态为关闭");
-            }
+        bt_menu.setOnClickListener(view -> {
+            showPopupMenu(bt_menu);
         });
-
-        turn_on.setOnClickListener(view -> {
-            //打开蓝牙
-            if (mBluetoothAdapter.isEnabled())
-                showToast("蓝牙已经打开了");
-            else{
-                //向系统请求开启蓝牙
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);//结果返回回调到onActivityResult()
-            }
-        });
-
-        turn_off.setOnClickListener(view -> {
-            //关闭蓝牙
-            if (!mBluetoothAdapter.isEnabled())
-                showToast("蓝牙已经关闭了");
-            else{
-                mBluetoothAdapter.disable();
-                showToast("成功关闭蓝牙");
-            }
-        });
-
 
     }
+
+    private void showPopupMenu(Button button) {
+        // View当前PopupMenu显示的相对View的位置
+        PopupMenu popupMenu = new PopupMenu(this, button);
+        // menu布局
+        popupMenu.getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
+        // menu的item点击事件
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.BlueTooth_check_state:
+                        //查看设备当前蓝牙状态
+                        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                        if (mBluetoothAdapter == null) {
+                            showToast("设备不支持蓝牙！");
+                        } else {
+                            if (mBluetoothAdapter.isEnabled())
+                                showToast("蓝牙状态为打开");
+                            else
+                                showToast("蓝牙状态为关闭");
+                        }
+                        break;
+                    case R.id.turn_on:
+                        //打开蓝牙
+                        if (mBluetoothAdapter.isEnabled())
+                            showToast("蓝牙已经打开了");
+                        else {
+                            //向系统请求开启蓝牙
+                            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);//结果返回回调到onActivityResult()
+                        }
+                        break;
+                    case R.id.turn_off:
+                        //关闭蓝牙
+                        if (!mBluetoothAdapter.isEnabled())
+                            showToast("蓝牙已经关闭了");
+                        else {
+                            mBluetoothAdapter.disable();
+                            showToast("成功关闭蓝牙");
+                        }
+                        break;
+                }
+                Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // PopupMenu关闭事件
+        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+                Toast.makeText(getApplicationContext(), "关闭PopupMenu", Toast.LENGTH_SHORT).show();
+            }
+        });
+        popupMenu.show();
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_ENABLE_BT)
+        if (requestCode == REQUEST_ENABLE_BT)
             showToast("成功打开蓝牙");
         else
             showToast("打开蓝牙失败");
